8f62d5c3b2c011606841cb3ae5254d70a0ae40bf
repositoriogsf <rodolfogomes@saofrancisco.com.br>
2017-12-21 11:31:47 -0200

    Commit inicial dos arquivos

---
+++ src/main/java/br/com/objetosbanco/api/ObjetosBancoApplication.java
@@ -0,0 +1,12 @@
+package br.com.objetosbanco.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetosBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetosBancoApplication.class, args);
+	}
+}
